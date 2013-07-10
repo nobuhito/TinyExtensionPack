61fcb6074d1f791a331f97c08db6b97b924844c2
nobuhito <nobuhito.sato@gmail.com>
2013-07-10 16:08:37 +0900

    改行コードの訂正

--- google_plus_bgcolor_change.js
+++ google_plus_bgcolor_change.js
@@ -1,3 +1,4 @@
+<<<<<<< .mine
 plugin['google_plus_bgcolor_change'] = {
     url: "https://plus.google.com",
     description: 'GooglePlusの背景色を変更する',
@@ -15,3 +16,22 @@ plugin['google_plus_bgcolor_change'] = {
         caption: '背景色を指定',
     }
 };
+=======
+plugin['google_plus_bgcolor_change'] = {
+    url: "https://plus.google.com",
+    description: 'GooglePlusの背景色を変更する',
+    styles: {
+        '.r6Rtbe': {
+            'borderTopWidth': '1px',
+        },
+        '.KTa': {
+            'backgroundColor': '$option$',
+        }
+    },
+    option: {
+        type: 'color',
+        value: '#ffffff',
+        caption: '背景色を指定',
+    }
+};
+>>>>>>> .theirs
--- google_plus_notification_icon_change.js
+++ google_plus_notification_icon_change.js
@@ -1,3 +1,4 @@
+<<<<<<< .mine
 var module = 'google_plus_notification_icon_change';
 plugin[module] = {
     url: [
@@ -15,3 +16,22 @@ plugin[module] = {
         },
     },
 };
+=======
+var module = 'google_plus_notification_icon_change';
+plugin[module] = {
+    url: [
+        "https://plus.google.com",
+        "https://mail.google",
+    ],
+    description: 'Google+の通知ベルのアイコンを稲妻に変更',
+    delay: 1000,
+    styles: {
+        '.gb_aa .gb_X': {
+            backgroundImage: "assets/Lightning_black.png",
+        },
+        ".gb_X": {
+            backgroundImage: "assets/Lightning_gray.png",
+        },
+    },
+};
+>>>>>>> .theirs
