24f1be3fe7d12acdb2c2b75d8edd5acbb75bc4ea
nobuhito <nobuhito.sato@gmail.com>
2013-08-02 20:16:43 +0900

    オプションが効いてなかった

--- google_plus_mark_for_public.js
+++ google_plus_mark_for_public.js
@@ -3,12 +3,12 @@ plugin[module] = {
     url: "https://plus.google.com",
     description: 'Google+の一般投稿コメント入力欄の色とキャプションを変更する',
     interval: 10000,
-    batch: function() {
+    batch: function(option) {
         var target = $(".B5:not(." + module + ")");
         target
 	          .addClass(module)
 	          .closest("div[id]").find(".Kj,.bI").each( function() {
-	              $(this).css('backgroundColor', '#F8F8F8').text('Public...');
+	              $(this).css('backgroundColor', option).text('Public...');
 	          });
     },
     option: {
